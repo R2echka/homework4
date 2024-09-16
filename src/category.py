21c4149451e8a92327976004f234229658a780c2
Reechka <R2echka@gmail.com>
2024-09-16 16:01:15 +0300

    some changes

--- src/category.py
+++ src/category.py
@@ -1,8 +1,10 @@
 '''Класс категорий'''
 class Category:
     category_count = 0
+    product_count = 0
     def __init__(self, name: str, desc: str, list: list):
         self.name = name
         self.desc = desc
         self.list = list
         Category.category_count += 1
+        self.product_count += len(list)
