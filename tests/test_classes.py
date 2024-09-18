102582410b78501e962e9e43f5a6972355fbf9e0
Reechka <R2echka@gmail.com>
2024-09-18 14:45:34 +0300

    lint fixes

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -10,7 +10,7 @@ def product() -> Product:
 
 
 @pytest.fixture()
-def category(product) -> Category:
+def category(product: Product) -> Category:
     return Category("name", "description", [product])
 
 
@@ -24,23 +24,26 @@ def test_product(product: Product) -> None:
 def test_category(category: Category) -> None:
     assert category.name == "name"
     assert category.description == "description"
-    assert category.products == ['name, 15.99 руб. Остаток: 100 шт.']
+    assert category.products == ["name, 15.99 руб. Остаток: 100 шт."]
     assert category.product_count == 1
 
 
 def test_number_of_categories() -> None:
     assert Category.category_count == 1
-    cat2 = Category('something', 'description', [])
+    cat2 = Category("something", "description", [])
     assert Category.category_count == 2
 
+
 def test_add_product(category: Category) -> None:
-    prod1 = Product('name1', '-', 1600, 1)
+    prod1 = Product("name1", "-", 1600, 1)
     category.add_product(prod1)
     assert category.product_count == 2
 
+
 def test_new_product() -> None:
-    prod2 = Product.new_product({'name': 'name1', 'description': '-', 'price': 43892.39, 'quantity': 432})
-    assert type(prod2) == Product
+    prod2 = Product.new_product({"name": "name1", "description": "-", "price": 43892.39, "quantity": 432})
+    assert type(prod2) is Product
+
 
 def test_price_setter(product: Product) -> None:
     product.price = -1
